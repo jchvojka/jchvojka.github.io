4aedff436ca937ff37f439f10cdefde7f4bb2687
chvojka <jesse.chvojka@waterfield.com>
2016-05-17 20:46:42 -0500

    Prepare for Heroku deploy

--- instagram/app/controllers/instagram_controller.rb
+++ instagram/app/controllers/instagram_controller.rb
@@ -1,9 +1,9 @@
 class InstagramController < ApplicationController
 
-  #client id: e74fe79c1eff4a7aac2ccbddb1d7c94d
   def index
+    # Test values
     # chvojkaj: 3242437786
     # ca: 647463488
-    @instagram = Instagram.user_recent_media(647463488, {:count => 999})
+    @instagram = Instagram.user_recent_media(647463488, {:count => 999})    
   end
 end
--- instagram/config/initializers/instagram.rb
+++ instagram/config/initializers/instagram.rb
@@ -1,9 +1,8 @@
 require "instagram"
 
 Instagram.configure do |config|
-  # To get access token
-  #https://instagram.com/oauth/authorize/?client_id=d2e2413cf242482d979ff11a026d0750&amp;redirect_uri=http://localhost:3000/instagram/index&amp;response_type=token
-  
-  config.client_id = "e74fe79c1eff4a7aac2ccbddb1d7c94d" # "d2e2413cf242482d979ff11a026d0750" # e74fe79c1eff4a7aac2ccbddb1d7c94d
-  # config.access_token = "3242437786.d2e2413.7adf0678149542fb8115b0ffccc8b789"
+  # Test values
+  # chvojkaj: d2e2413cf242482d979ff11a026d0750
+  # ca: e74fe79c1eff4a7aac2ccbddb1d7c94d
+  config.client_id = "e74fe79c1eff4a7aac2ccbddb1d7c94d" 
 end
