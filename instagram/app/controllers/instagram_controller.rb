bd5718e76bfec2ebbab373e0092c139cd9a47370
chvojka <jesse.chvojka@waterfield.com>
2016-05-17 23:18:25 -0500

    Load instagram pictures into a database

--- instagram/app/controllers/instagram_controller.rb
+++ instagram/app/controllers/instagram_controller.rb
@@ -4,6 +4,19 @@ class InstagramController < ApplicationController
     # Test values
     # chvojkaj: 3242437786
     # ca: 647463488
-    @instagram = Instagram.user_recent_media(647463488, {:count => 999})    
+    @instagram = Instagram.user_recent_media(647463488, {:count => 999})
+    @prior_instagram_ids = Photo.pluck(:instagram_id)
+    @instagram.each do |pic|
+      if ( not @prior_instagram_ids.include?(pic.try(:id)) ) && pic.tags.include?("consumeraffairs")
+        photo = Photo.new
+        photo.data = pic.images.standard_resolution.url
+        photo.text = pic.caption.text
+        photo.author = pic.user.full_name
+        photo.instagram_id = pic.try(:id)
+        photo.save!
+      end
+    end
+    @photos = Photo.all
+
   end
 end
--- instagram/db/migrate/20160515172554_create_photos.rb
+++ instagram/db/migrate/20160515172554_create_photos.rb
@@ -1,13 +1,18 @@
 class CreatePhotos < ActiveRecord::Migration
-  def change
+  def up
     create_table :photos do |t|
+      t.string :name
       t.string :title
       t.text :text
-      t.string :name
       t.string :author
-      t.string :client_id
+      t.binary :data
+      t.string :instagram_id
 
       t.timestamps
     end
+  end
+
+  def down
+    drop_table :photos
   end
 end
